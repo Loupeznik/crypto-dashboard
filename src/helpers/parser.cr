147f2b3377316a9b6d1d4d9d099c9bc51ef4d311
Dominik Zarsky <dominik.zarsky@gmail.com>
2021-04-12 15:41:48 +0200

    complete parser functionality

--- src/helpers/parser.cr
+++ src/helpers/parser.cr
@@ -3,24 +3,33 @@ require "json"
 
 # Parses data for given crypto
 class Parser
+  @coin : String
 
-    @coin : String
+  def initialize(@coin)
+  end
 
-    def initialize(@coin)
-    end
+  property :coin
 
-    property :coin
-
-    # Fetch data from messario API
-    def fetchData
+  # Fetch data from messario API
+  def fetchData
     response = Halite.get("https://data.messari.io/api/v1/assets/#{@coin}/metrics")
-        return response.parse("json")
-    end
+    return response.parse("json")
+  end
 
-    # Parse data into an array for further processing
-    def parseData
-		data = fetchData
-        puts data
-    end
-
+  # Parse data into an array for further processing
+  def parseData
+    data = fetchData
+    values = [
+      data["data"]["name"],                     # Crypto name
+      data["data"]["symbol"],                   # Crypto symbol
+      data["data"]["market_data"]["price_usd"], # Current price in USD
+      data["data"]["market_data"]["percent_change_usd_last_1_hour"], # Price change in % in last hour
+      data["data"]["market_data"]["ohlcv_last_1_hour"]["low"], # Lowest price in last hour
+      data["data"]["market_data"]["ohlcv_last_1_hour"]["high"], # Highest price in last hour
+      data["data"]["market_data"]["percent_change_usd_last_24_hours"], # Price change in % in last day
+      data["data"]["market_data"]["ohlcv_last_24_hour"]["low"], # Lowest price in last day
+      data["data"]["market_data"]["ohlcv_last_24_hour"]["high"], # Highest price in last day
+    ]
+    return values
+  end
 end
